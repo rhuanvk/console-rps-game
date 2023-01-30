9b15e8278df19b93e3b5767b07696059d4e2cb1c
rhuanvk <rhuanvk@gmail.com>
2023-01-30 16:52:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+const choices = ['Rock', 'Paper', 'Scissors'];
+let result;
+let computerChoice;
+let playerScore = 0;
+let computerScore = 0;
+
+let maxScore = Number(prompt("Let's play Rock, Paper, Scissors! Do you want to play 3 or 5 rounds?"));
+
+/* if (maxScore = 3 || maxScore = 5) {
+    maxScore = (prompt("Please inform a valid number of rounds that you wanna play (3 or 5)."));
+    Number(maxScore);
+} */
+
+alert("If you don't want to play anymore, just type 'exit'.");
+
+function playerPlay() {
+    playerChoice = prompt("Enter your choice:");
+    playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1);
+}
+
+function computerPlay() {
+    let randomNum = Math.floor(Math.random() * 3);
+    return computerChoice = choices[randomNum];
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (!(choices.includes(playerChoice))) {
+        alert('[Error!] Please type something valid (rock, paper or scissors).');
+        console.log(playerChoice);
+        return;
+    }
+
+    if (playerChoice == computerChoice) {
+        result = "It's a tie. Let's play again!";
+        alert(result);
+        return;
+    }
+    else if (
+        (playerChoice == 'Rock' && computerChoice == 'Scissors') ||
+        (playerChoice == 'Paper' && computerChoice == 'Rock') ||
+        (playerChoice == 'Scissors' && computerChoice == 'Paper')) {
+        result = `Computer chose ${computerChoice}. ${playerChoice} beats ${computerChoice}. You score! `;
+        alert(result);
+        playerScore++;
+        return;
+    }
+    else {
+        result = `Computer chose ${computerChoice}. ${computerChoice} beats ${playerChoice}. Computer score!`;
+        alert(result);
+        computerScore++;
+        return;
+    }
+}
+
+game();
+
+function game() {
+    for (; playerScore <= maxScore || playerScore <= maxScore;) {
+        playerPlay();
+        if (playerChoice == 'Exit') {
+            alert('Exiting now!');
+            return;
+        }
+
+        playRound(playerChoice, computerPlay());
+        scoreboard = `Score: ${playerScore} x ${computerScore}.`;
+        alert(scoreboard);
+        if (playerScore == maxScore) {
+            scoreboard = 'Congratulations, you win!';
+            alert(scoreboard);
+            return;
+        }
+        else if (computerScore == maxScore) {
+            scoreboard = 'Game over! Computer wins.';
+            alert(scoreboard);
+            return;
+        }
+    }
+}
