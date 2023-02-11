e7af098e3f171dbb77ee7d9edb94537d16ad7c58
granitnebiu <granitnebiu@gmail.com>
2023-02-11 16:53:10 +0100

    coment

--- script.js
+++ script.js
@@ -1,75 +1,74 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ["Rock", "Paper", "Scissors"];
 let result;
 let computerChoice;
 let playerScore = 0;
 let computerScore = 0;
 
-alert("Let's play Rock, Paper, Scissors! First player to get 5 points wins the game.");
-
-alert("If you don't want to play anymore, just type 'exit'.")
+alert(
+   "Let's play Rock, Paper, Scissors! First player to get 5 points wins the game."
+);
+//coment
+alert("If you don't want to play anymore, just type 'exit'.");
 
 function playerPlay() {
-    playerChoice = prompt("Enter your choice (rock, paper or scissors):");
-    playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1);
+   playerChoice = prompt("Enter your choice (rock, paper or scissors):");
+   playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1);
 }
 
 function computerPlay() {
-    let randomNum = Math.floor(Math.random() * 3);
-    return computerChoice = choices[randomNum];
+   let randomNum = Math.floor(Math.random() * 3);
+   return (computerChoice = choices[randomNum]);
 }
 
 function playRound(playerChoice, computerChoice) {
-    if (!(choices.includes(playerChoice))) {
-        alert('[Error!] Please type something valid (rock, paper or scissors).');
-        console.log(playerChoice);
-        return;
-    }
+   if (!choices.includes(playerChoice)) {
+      alert("[Error!] Please type something valid (rock, paper or scissors).");
+      console.log(playerChoice);
+      return;
+   }
 
-    if (playerChoice == computerChoice) {
-        result = "It's a tie. Let's play again!";
-        alert(result);
-        return;
-    }
-    else if (
-        (playerChoice == 'Rock' && computerChoice == 'Scissors') ||
-        (playerChoice == 'Paper' && computerChoice == 'Rock') ||
-        (playerChoice == 'Scissors' && computerChoice == 'Paper')) {
-        result = `Computer chose ${computerChoice}. ${playerChoice} beats ${computerChoice}. You score!`;
-        alert(result);
-        playerScore++;
-        return;
-    }
-    else {
-        result = `Computer chose ${computerChoice}. ${computerChoice} beats ${playerChoice}. Computer score!`;
-        alert(result);
-        computerScore++;
-        return;
-    }
+   if (playerChoice == computerChoice) {
+      result = "It's a tie. Let's play again!";
+      alert(result);
+      return;
+   } else if (
+      (playerChoice == "Rock" && computerChoice == "Scissors") ||
+      (playerChoice == "Paper" && computerChoice == "Rock") ||
+      (playerChoice == "Scissors" && computerChoice == "Paper")
+   ) {
+      result = `Computer chose ${computerChoice}. ${playerChoice} beats ${computerChoice}. You score!`;
+      alert(result);
+      playerScore++;
+      return;
+   } else {
+      result = `Computer chose ${computerChoice}. ${computerChoice} beats ${playerChoice}. Computer score!`;
+      alert(result);
+      computerScore++;
+      return;
+   }
 }
-
 
 game();
 
 function game() {
-    for (; playerScore <= 5 || computerScore <= 5;) {
-        playerPlay();
-        if (playerChoice == 'Exit') {
-            alert('Exiting now!');
-            return;
-        }
+   for (; playerScore <= 5 || computerScore <= 5; ) {
+      playerPlay();
+      if (playerChoice == "Exit") {
+         alert("Exiting now!");
+         return;
+      }
 
-        playRound(playerChoice, computerPlay());
-        scoreboard = `Score: ${playerScore} x ${computerScore}.`;
-        alert(scoreboard);
-        if (playerScore == 5) {
-            scoreboard = 'Congratulations, you win!';
-            alert(scoreboard);
-            return;
-        }
-        else if (computerScore == 5) {
-            scoreboard = 'Game over! Computer wins.';
-            alert(scoreboard);
-            return;
-        }
-    }
+      playRound(playerChoice, computerPlay());
+      scoreboard = `Score: ${playerScore} x ${computerScore}.`;
+      alert(scoreboard);
+      if (playerScore == 5) {
+         scoreboard = "Congratulations, you win!";
+         alert(scoreboard);
+         return;
+      } else if (computerScore == 5) {
+         scoreboard = "Game over! Computer wins.";
+         alert(scoreboard);
+         return;
+      }
+   }
 }
